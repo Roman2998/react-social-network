a4297c9ff8cd8d6182a3fbac8be1cd24539661dc
Roman <Roman290698@gmail.com>
2021-10-15 23:11:58 +0300

    Preloader creation

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,81 +1,64 @@
 import React from "react";
-import s from './Users.module.css';
-import axios from "axios";
-import userPhoto from '../../assets/images/user.png'
+import s from "./Users.module.css";
+import userPhoto from "../../assets/images/user.png";
 
-class Users extends React.Component {
+let Users = (props) => {
 
-	componentDidMount() {
-		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-			.then(response => {
-				this.props.setUsers(response.data.items);
-				this.props.setTotalUsersCount(response.data.totalCount);
-			});
-	}
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-	onPageChanged = (pageNumber) => {
-		this.props.setCurrentPage(pageNumber);
-		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-			.then(response => {
-				this.props.setUsers(response.data.items)
-			});
-	}
+    let pages = [];
+    /*for (let i=1; i <= pagesCount; i++) {
+        pages.push(i)
+    }*/
+    if (pagesCount > 10) {
+        if (props.currentPage > 5) {
+            pages.push(1, '...')
+            for (let i = props.currentPage - 4; i <= props.currentPage + 5; i++) {
+                pages.push(i)
+                if (i === props.currentPage + 5) pages.push('...', pagesCount)
+                if (i === pagesCount) break
+            }
+        } else {
+            for (let i = 1; i <= 10; i++) {
+                pages.push(i)
+                if (i === 10) pages.push('...', pagesCount)
+                if (i === pagesCount) break
+            }
+        }
+    } else {
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+    }
 
-	render() {
+    return <div>
+        <div>
+            {pages.map(p => {
+                return <span className={props.currentPage === p && s.selectedPage}
+                             onClick={(e) => {
+                                 props.onPageChanged(p)
+                             }}>{p}</span>
+            })}
 
-		let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-		let pages = [];
-		/*for (let i=1; i <= pagesCount; i++) {
-			pages.push(i)
-		}*/
-		if (pagesCount > 10) {
-			if (this.props.currentPage > 5) {
-				pages.push(1, '...')
-				for (let i = this.props.currentPage - 4; i <= this.props.currentPage + 5; i++) {
-					pages.push(i)
-					if (i === this.props.currentPage + 5) pages.push('...', pagesCount)
-					if (i === pagesCount) break
-				}
-			} else {
-				for (let i = 1; i <= 10; i++) {
-					pages.push(i)
-					if (i === 10) pages.push('...', pagesCount)
-					if (i === pagesCount) break
-				}
-			}
-		} else {
-			for (let i = 1; i <= pagesCount; i++) {
-				pages.push(i)
-			}
-		}
-
-		return <div>
-			<div>
-				{ pages.map( p => {
-					return <span className={ this.props.currentPage === p && s.selectedPage }
-					onClick={ (e) => { this.onPageChanged(p) } }>{p}</span>
-				})}
-
-			</div>
-			{
-				this.props.users.map(u => <div key={u.id}>
+        </div>
+        {
+            props.users.map(u => <div key={u.id}>
 				<span>
 					<div>
 						<img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto}/>
 					</div>
 					<div>
 						{u.followed
-							? <button onClick={() => {
-								this.props.unfollow(u.id)
-							}}>Unfollow</button>
-							: <button onClick={() => {
-								this.props.follow(u.id)
-							}}>Follow</button>}
+                            ? <button onClick={() => {
+                                props.unfollow(u.id)
+                            }}>Unfollow</button>
+                            : <button onClick={() => {
+                                props.follow(u.id)
+                            }}>Follow</button>}
 
 					</div>
 				</span>
-					<span>
+                <span>
 					<span>
 						<div>{u.name}</div>
 						<div>{u.status}</div>
@@ -85,10 +68,8 @@ class Users extends React.Component {
 						<div>{'u.location.city'}</div>
 					</span>
 				</span>
-				</div>)
-			}
-		</div>
-	}
+            </div>)
+        }
+    </div>
 }
-
 export default Users;
